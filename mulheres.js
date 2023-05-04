5b43f53b1ab9f39dd7b5659c7d661c9cca61d823
maybeduda <dudas.soares21@gmail.com>
2023-05-04 12:29:39 -0300

    Insere a nova versão do projeto - MongoDB

--- mulheres.js
+++ mulheres.js
@@ -1,32 +1,83 @@
-const porta = 3333;
-const express = require("express");
-const app = express();
-const router = express.Router();
+const porta = 3333;//criando a porta
+const express = require("express"); //iniciando o express
+const app = express();//iniciando o app
+const router = express.Router();//configurando a primeira parte da rota
+const cors = require("cors")//trazendo pacote cors que permite a API no front
+app.use(express.json());
+app.use(cors());
+const conectaBancoDeDados = require('./bancoDeDados');//ligando ao arquivo banco de dados
+conectaBancoDeDados()//chamando a função que conecta o banco de dados
+const Mulher = require('./mulherModel');
+//GET
+    async function mostraMulheres(request, response){
+        try {
+            const mulheresVindas = await Mulher.find()
+            response.json(mulheresVindas)
+        }catch (erro){
+            console.log(erro)
 
-const mulheres = [
-    {
-        nome: 'Duda Soares',
-        imagem: 'https://media.licdn.com/dms/image/D4D03AQE5v72B3eOKPQ/profile-displayphoto-shrink_200_200/0/1680632598050?e=1687996800&v=beta&t=BFBKtm9FrHIiqqLxB-KEUQTbBrUvBHLlyne1U-vWdfQ',
-        minibio: 'Futura Dev'
-    },
-    {
-        nome: 'Iana Chan',
-        imagem: 'https://media.licdn.com/dms/image/C4D03AQE-aD7nj2W_0Q/profile-displayphoto-shrink_800_800/0/1563383651406?e=2147483647&v=beta&t=q65Pubo5FqXxPYUI81nfsp6vcAnv5Ybz5EJjR5AGBLo',
-        minibio: 'Fundadora progaMaria'
-    },
-    {
-        nome: 'Nina da Hora',
-        imagem: 'https://media.licdn.com/dms/image/C4D03AQE-aD7nj2W_0Q/profile-displayphoto-shrink_800_800/0/1563383651406?e=2147483647&v=beta&t=q65Pubo5FqXxPYUI81nfsp6vcAnv5Ybz5EJjR5AGBLo',
-        minibio: 'Hacker antirracista'
-    }
-]
-
-    function mostraMulheres(request, response){
-        response.json(mulheres);
+        }
     }
 
+//post
+    async function criaMulher(request, response) {
+        const novaMulher = new Mulher ({
+            nome: request.body.nome,
+            imagem: request.body.imagem,
+            minibio: request.body.minibio,
+            citacao: request.body.citacao
+        })
+
+        try {
+            const mulherCriada = await novaMulher.save()
+            response.status(201).json(mulherCriada);
+        } catch (erro) {
+            console.log(erro)
+        }
+    }
+
+//patch
+    async function corrigeMulher(request, response) {
+        try {
+                const mulherEncontrada = await Mulher.findById(request.params.id)
+            
+                if (request.body.nome) {
+                    mulherEncontrada.nome = request.body.nome
+                }
+                if (request.body.minibio) {
+                    mulherEncontrada.minibio = request.body.minibio
+                }
+                if (request.body.imagem) {
+                    mulherEncontrada.imagem = request.body.imagem
+                }
+                if(request.body.citacao) {
+                    mulherEncontrada.citacao = request.body.citacao
+                }
+                const mulherAtualizada = await mulherEncontrada.save()
+                response.json(mulherAtualizada);
+            
+            } catch (erro) {
+                console.log(erro)
+            }
+    }
+
+//delete
+    async function deletaMulher(request, response) {
+        try {
+            await Mulher.findByIdAndDelete(request.params.id)
+            response.json({messagem: "Mulher deletada com sucesso!"})
+        } catch (erro) {
+            console.log (erro)
+        }
+    }
+
+//porta
     function mostraPorta() {
         console.log("Servidor criado e rodando na porta", porta);
     }
-app.use(router.get('/mulheres', mostraMulheres));
-app.listen(porta, mostraPorta);
+
+app.use(router.get('/mulheres', mostraMulheres));//configurei rota GET /mulheres
+app.use(router.post('/mulheres', criaMulher));//configurei rota post /mulheres
+app.use(router.patch('/mulheres/:id', corrigeMulher))//configurei a rota patch /mulheres/:id
+app.use(router.delete ('/mulheres/:id', deletaMulher));//configurei a rota delete /mulheres
+app.listen(porta, mostraPorta); //servidor da porta
